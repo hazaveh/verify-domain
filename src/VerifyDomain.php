dbafd460bc1fdba045498a9d9537264b84e592e6
Mahdi Hazaveh <me@hazaveh.net>
2023-10-02 12:28:39 +0200

    Initial 🚀

---
+++ src/VerifyDomain.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Hazaveh\VerifyDomain;
+
+use Hazaveh\VerifyDomain\Contracts\HandlerInterface;
+use Hazaveh\VerifyDomain\Handler\LookupHandler;
+
+class VerifyDomain
+{
+    /**
+     * @var HandlerInterface $handler
+     */
+    private $handler;
+    public function __construct($handler = null)
+    {
+        $this->handler = $handler ?: new LookupHandler();
+    }
+
+    /**
+     * @param string $domain
+     * @param string $value
+     * @return bool
+     */
+    public function verifyByDNS(string $domain, string $value)
+    {
+        try {
+            $records = $this->handler->dns_get_record($domain, DNS_TXT);
+        } catch (\Exception $e) {
+            $records = [];
+        }
+
+        $filteredRecords = [];
+        foreach ($records as $record) {
+            if ($record['txt'] === $value && $record['hostname'] === $domain) {
+                $filteredRecords[] = $record;
+            }
+        }
+        return !empty($filteredRecords);
+    }
+
+    /**
+     * @param $domain
+     * @param $file
+     * @param $value
+     * @return bool
+     */
+    public function verifyByFile($domain, $file, $value)
+    {
+        $url = "http://$domain/$file";
+
+        return trim($this->handler->file_get_contents($url)) == $value;
+    }
+}
